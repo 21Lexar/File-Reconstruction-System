4a8522043992fb4154c5e885f08a19861997fcad
irab-zara <irabzara@gmail.com>
2025-10-12 16:07:01 +0500

    Made header file, implementation file and modified main

--- Deliverable-2/main.cpp
+++ Deliverable-2/main.cpp
@@ -1,53 +1,35 @@
-#include <string>
+#include "DataStructures.h" 
 #include <iostream>
+#include <string>
 #include <filesystem>
+#include <stdexcept> 
 namespace fs = std::filesystem;
 using namespace std;
 
+// The Link List's starting pointer must be defined here.
+//This is so we can make global changes across all 3 files. 
+node *activeHead = NULL; 
+
 const string PATH = fs::current_path() / "Temp";
 
-struct node{
-    string data;
-    node* next;
-};
-node *head = NULL;
+//Function prototypes for compiler
+void append(string filePath, node*& head);
+void printList(node* head);
 
-void append(string filePath,node* &head=head){
-   node* ptr = new node;
-   node* temp = head;
-   ptr->data = filePath;
-   ptr->next = NULL;    
-
-   if (head == NULL) {
-      head = ptr;
-      return;
-   }
-
-   while (temp->next != NULL) {
-      temp = temp->next;
-   }
-   temp->next = ptr;
-}
-
-void printList(node* head=head){
-    if(!head) {
-    cerr << "Error: Head is NULL\n";
-    }else{
-        node* ptr = head;
-        while(ptr!=NULL){
-            cout << ptr->data << endl;
-            cout << "|\nv\n";
-            ptr=ptr->next; 
-        }
-        cout << "END\n";
-    }
-}
-
-int main()
-{   
-    // Iterator inside directory
+int main() {
+    
+    //Stack object
+    RecoveryStack recoveryStack; 
+    
+    //File scanning:
+    cout << "Scanning directory for files: " << PATH << endl;
+    
+    // Iterator inside directory 
     for (const auto & entry : fs::directory_iterator(PATH)){
-        append(entry.path());
+        append(entry.path().string(), activeHead); 
+        //.string() added since w/o it this was returning the path object instead of name
     }
-    printList();
+    
+    printList(activeHead); 
+
 }
