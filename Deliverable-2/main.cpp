ec95707a6ee2aec6e9b8752a16e9f3a82933b6ae
21Lexar <The-charon@hotmail.com>
2025-10-12 15:34:20 +0500

    Link main to header file

--- Deliverable-2/main.cpp
+++ Deliverable-2/main.cpp
@@ -1,53 +1,18 @@
 #include <string>
 #include <iostream>
 #include <filesystem>
+#include "include/dsa.h"
 namespace fs = std::filesystem;
 using namespace std;
 
 const string PATH = fs::current_path() / "Temp";
-
-struct node{
-    string data;
-    node* next;
-};
 node *head = NULL;
-
-void append(string filePath,node* &head=head){
-   node* ptr = new node;
-   node* temp = head;
-   ptr->data = filePath;
-   ptr->next = NULL;    
-
-   if (head == NULL) {
-      head = ptr;
-      return;
-   }
-
-   while (temp->next != NULL) {
-      temp = temp->next;
-   }
-   temp->next = ptr;
-}
-
-void printList(node* head=head){
-    if(!head) {
-    cerr << "Error: Head is NULL\n";
-    }else{
-        node* ptr = head;
-        while(ptr!=NULL){
-            cout << ptr->data << endl;
-            cout << "|\nv\n";
-            ptr=ptr->next; 
-        }
-        cout << "END\n";
-    }
-}
 
 int main()
 {   
     // Iterator inside directory
     for (const auto & entry : fs::directory_iterator(PATH)){
-        append(entry.path());
+        append(entry.path(),head);
     }
-    printList();
+    printList(head);
 }
