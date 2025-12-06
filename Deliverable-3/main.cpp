ae3da81dfbd49e88a13f9b30be4d040c86b920b0
irab-zara <irabzara@gmail.com>
2025-12-06 19:49:01 +0500

    Modifed main to remove linked list implementation

--- Deliverable-3/main.cpp
+++ Deliverable-3/main.cpp
@@ -1,68 +1,15 @@
 #include "include/DataStructures.h" 
 #include <iostream>
 #include <string>
-#include <filesystem>
-#include <stdexcept>
-
-
-namespace fs = std::filesystem;
 using namespace std;
 
-// The Link List's starting pointer must be defined here.
-//This is so we can make global changes across all 3 files. 
-node *activeHead = NULL; 
-
-const string PATH = (fs::current_path() / "Temp").string();
-
-//Function prototypes for compiler
-void append(string filePath, node*& head);
-void printList(node* head);
-
-void clearScreen(){
-	cout << "\033[2J\033[1;1H"; //ANSI Sequence to clear screen
-}
-
-void displayMenu(){
-	cout << "File Reconstruction System\n";
-	cout << "1. Delete\n";
-	cout << "2. Recover\n";
-	cout << "3. List\n";
-	cout << "4. Quit\n";
-}
-
-void getUserChoice(){
-	int choice;
-	cout << "Enter your choice: "; cin >> choice;
-	switch(choice){
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3: 
-			break;
-		case 4: 
-			break;
-		default:
-			clearScreen();
-			displayMenu();
-			getUserChoice();
-	}
-}
-
 int main() {
-	
-	//Stack object
-	RecoveryStack recoveryStack;
+    
+    // Object contain AVLTree and RecoveryStack
+    FileManager manager;
 
-	cout << "Scanning directory for files: " << PATH << endl;
-	// Iterator inside directory 
-	for (const auto & entry : fs::directory_iterator(PATH)){
-		append(entry.path().string(), activeHead); 
-	}
-	
-	printList(activeHead); 
-	
-	// To run the program, run build.sh and then run FRS
-
-	
+    cout << "Digital Forensic File Reconstruction System (FRS) Initialized." << endl;
+    
+    // Call menu loop
+    manager.menu();
 }
