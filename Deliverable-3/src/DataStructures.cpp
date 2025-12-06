261e0978729a263bdcbaf69f3b57a7108d7bb9d3
irab-zara <irabzara@gmail.com>
2025-12-06 16:17:41 +0500

    fix: Implemented RecoveryStack methods using linked list logic

--- Deliverable-3/src/DataStructures.cpp
+++ Deliverable-3/src/DataStructures.cpp
@@ -5,80 +5,55 @@ using namespace std;
 
 RecoveryStack stackData;
 
-void RecoveryStack::push(const File& f) {
+// Implementation using Linked List instead of Stack
 
-    cout << " Metadata for " << f.name << " pushed to stack.\n";
+bool RecoveryStack::isEmpty() const {
+    return top == nullptr; // O(1) complexity
 }
 
+// PUSH: Add at head of list
+void RecoveryStack::push(const File& f) {
+    node* newNode = new node;
+    newNode->file = f;
+    newNode->next = top; // newNode points to current top
+    top = newNode;       // newNode becomes new top
+    cout << " Metadata for " << f.name << " pushed to recovery stack.\n";
+}
+
+// POP: Remove from head of list
 File RecoveryStack::pop() {
     if (isEmpty()) {
         cerr << "Error! Recovery Stack is empty.\n";
-        // we have to return a File object, for now we return a dummy one
-        return File("NULL", "NULL", "NULL"); 
+        // Returns dummy object (this must include the ID)
+        return File(0, "NULL", "NULL", "NULL"); 
     }
-    
-    // First, use vector's basic array access [] to *get* the last element
-    File lastDeleted = stackData[stackData.size() - 1];
-    
-    // Then, actually pop the element
+
+    node* oldTop = top;
+    File lastDeleted = oldTop->file;
+
+    top = top->next; // point top down to the next node 
+    delete oldTop;   // free memory of the popped node
 
     return lastDeleted;
 }
 
-bool RecoveryStack::isEmpty() const {
-
-}
-
 void RecoveryStack::displayHistory() const {
-    
     if (isEmpty()) {
-        cout << "\n- Recovery History -\n";
-        cout << "[No Deleted Files in History]\n";
+        cout << "\n--- Recovery History ---\n";
+        cout << "[No Deleted Files in History Yet]\n";
         return;
     }
 
-    cout << "\n - Recovery History -\n";
-    
-    // 2. Loop from the top of stack to bottom
-    for (int i = stackData.size() - 1; i >= 0; --i) {
-        
-        //The serial number 
-        int sn = stackData.size() - i; 
-        
-        cout << "    " <<sn << ". " << stackData[i].name << " Path: " << stackData[i].path << "\n";
-    }
-}
+    cout << "\n ~ Recovery History ~\n";
 
+    node* current = top;
+    int sn = 1;
 
-// I moved linked list functions from main.cpp file
+    // looping from top of stack to bottom
+    while(current != nullptr) {
 
-void append(string filePath, node* &head) { 
-    node* ptr = new node;
-    node* temp = head;
-    ptr->data = filePath;
-    ptr->next = NULL; 
-    
-    if (head == NULL) {
-        head = ptr;
-        return;
-    }
-
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = ptr;
-}
-
-void printList(node* head){
-    if(!head) {
-    cerr << "Error: Head is NULL\n";
-    }else{
-        node* ptr = head;
-        while(ptr!=NULL){
-            cout << ptr->data << endl;
-            cout << "|\nv\n";
-            ptr=ptr->next; 
-        }
-        cout << "END\n";
+        cout << "    " << sn << ". " << current->file.name << " ID: " << current->file.id << " Path: " << current->file.path << "\n";
+        current = current->next; 
+        sn++;
     }
 }
